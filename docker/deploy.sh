6dbfd18822efa9c06955662b4210fd00a5c7c44c
AlexHerrmann(josojo) <josojo@hotmail.de>
2021-10-06 12:56:02 +0200

    adding docker setup for python cronjobs (gnosis/gp-dune-bridge#13)

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -7,3 +7,9 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_NAME" --password-stdin
 docker build -t "${DOCKERHUB_PROJECT}" -f docker/Dockerfile.binary .
 docker tag "${DOCKERHUB_PROJECT}" gnosispm/"${DOCKERHUB_PROJECT}":$1
 docker push gnosispm/"${DOCKERHUB_PROJECT}":$1
+
+# Build and tag dune-data-fetcher-image
+cd dune_data_api
+docker build -t "${DOCKERHUB_PROJECT}-fetcher" -f docker/Dockerfile.binary .
+docker tag "${DOCKERHUB_PROJECT}-fetcher" gnosispm/"${DOCKERHUB_PROJECT}-fetcher":$1
+docker push gnosispm/"${DOCKERHUB_PROJECT}-fetcher":$1
