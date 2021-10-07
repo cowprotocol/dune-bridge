fbf19e3684c9c6c23f31cc6b989110ae5186d84c
AlexHerrmann(josojo) <josojo@hotmail.de>
2021-10-07 17:51:33 +0200

    initialize tracing correctly (gnosis/gp-dune-bridge#16)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use gpdata::dune_data_loading::load_dune_data_into_memory;
 use gpdata::in_memory_maintenance::in_memory_database_maintaince;
 use gpdata::models::in_memory_database::InMemoryDatabase;
 use gpdata::serve_task;
+use gpdata::tracing_helper::initialize;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -9,6 +10,8 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Arguments {
+    #[structopt(long, env = "LOG_FILTER", default_value = "warn,debug,info")]
+    pub log_filter: String,
     #[structopt(long, env = "BIND_ADDRESS", default_value = "0.0.0.0:8080")]
     bind_address: SocketAddr,
     #[structopt(long, env = "DUNE_DATA_FILE", default_value = "./user_data.json")]
@@ -18,7 +21,8 @@ struct Arguments {
 #[tokio::main]
 async fn main() {
     let args = Arguments::from_args();
-    tracing::debug!("running data-server with {:#?}", args);
+    initialize(args.log_filter.as_str());
+    tracing::info!("running data-server with {:#?}", args);
     let dune_download_file = args.dune_data_file;
 
     let dune_data = load_dune_data_into_memory(dune_download_file.clone())
