f0d4475dcce0b3df9d2de01bbf388677e04b8bc3
josojo <josojo@hotmail.de>
2021-10-07 08:37:32 +0200

    fix PR for deployment script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -9,7 +9,7 @@ docker tag "${DOCKERHUB_PROJECT}" gnosispm/"${DOCKERHUB_PROJECT}":$1
 docker push gnosispm/"${DOCKERHUB_PROJECT}":$1
 
 # Build and tag dune-data-fetcher-image
-cd dune_data_api
+cd dune_api_scripts
 docker build -t "${DOCKERHUB_PROJECT}-fetcher" -f docker/Dockerfile.binary .
 docker tag "${DOCKERHUB_PROJECT}-fetcher" gnosispm/"${DOCKERHUB_PROJECT}-fetcher":$1
 docker push gnosispm/"${DOCKERHUB_PROJECT}-fetcher":$1
