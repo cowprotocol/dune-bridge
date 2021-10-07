63b7416bfedb9ed3f92bfa68dd588093a90e4a2f
AlexHerrmann(josojo) <josojo@hotmail.de>
2021-10-07 13:32:05 +0200

    adding basic api functionality (gnosis/gp-dune-bridge#14)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use gpdata::dune_data_loading::load_dune_data_into_memory;
+use gpdata::models::in_memory_database::InMemoryDatabase;
+use gpdata::serve_task;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::sync::Mutex;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Arguments {
+    #[structopt(long, env = "BIND_ADDRESS", default_value = "0.0.0.0:8080")]
+    bind_address: SocketAddr,
     #[structopt(long, env = "DUNE_DATA_FILE", default_value = "./user_data.json")]
     dune_data_file: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Arguments::from_args();
     tracing::info!("running data-server with {:#?}", args);
     let dune_data =
         load_dune_data_into_memory(args.dune_data_file).expect("could not load data into memory");
-    println!("{:?}", dune_data);
+    let memory_database = Arc::new(InMemoryDatabase(Mutex::new(dune_data)));
+
+    let serve_task = serve_task(memory_database.clone(), args.bind_address);
+    tokio::select! {
+        result = serve_task => tracing::error!(?result, "serve task exited"),
+    };
 }
