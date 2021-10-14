305a68a71f8fcc2138ebc8b056e6d05dda8421d3
josojo <josojo@hotmail.de>
2021-10-14 09:40:55 +0200

    [trival] formatting in startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,3 @@
-  
 #!/bin/sh
 # The purpose of this script is to set a log regex to forward error logs to stderr and other logs to
 # stdout. This allows us to easily configure alerts when any message goes to stderr.
