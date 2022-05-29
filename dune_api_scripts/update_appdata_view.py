b2ac0f9e3ff7327d6e144fb3309042098e590817
Ben Smith <bh2smith@gmail.com>
2022-05-29 09:37:15 +0200

    fix update app data scrit and add follow up query

--- dune_api_scripts/update_appdata_view.py
+++ dune_api_scripts/update_appdata_view.py
@@ -3,6 +3,8 @@ from os import getenv
 
 from duneapi.api import DuneAPI
 from duneapi.types import DuneQuery, Network
+from duneapi.util import open_query
+
 from .utils import app_data_entries
 
 if __name__ == "__main__":
@@ -16,10 +18,28 @@ if __name__ == "__main__":
     dune_user_generated.gp_appdata (app_data, referrer)
     AS VALUES {VALUES};"""
 
-    # update query in dune
-    query_id = int(getenv("QUERY_ID_ALL_APP_DATA", "257782"))
+    app_data_query = DuneQuery(
+        name="App Data Mapping",
+        description="",
+        raw_sql=QUERY,
+        network=Network.MAINNET,
+        parameters=[],
+        query_id=int(getenv("QUERY_ID_ALL_APP_DATA", "257782"))
+    )
+    # App hash with referral data as json
+    dune.initiate_query(app_data_query)
+    dune.execute_query(app_data_query)
+    # Check out the raw results here: https://dune.xyz/queries/257782
 
-    dune_query = DuneQuery("", "", QUERY, Network.MAINNET, [], query_id)
-    # fetch data
-    dune.execute_query(dune_query)
-    # Check out the results here: https://dune.xyz/queries/257782
+    # Parsed referral data
+    parsed_app_data_query = DuneQuery(
+        name="Parsed App Data Mapping",
+        description="",
+        raw_sql=open_query('./dune_api_scripts/queries/parsed_app_data.sql'),
+        network=Network.MAINNET,
+        parameters=[],
+        query_id=int(getenv("QUERY_ID_PARSED_APP_DATA", "279621"))
+    )
+    # TODO - update execute_query to only need query ID and parameters.
+    dune.execute_query(parsed_app_data_query)
+    # Check out the parsed results here: https://dune.xyz/queries/279621
