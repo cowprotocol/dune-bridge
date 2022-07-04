3656c8e48604e35dcf675d0511c15429e7d83918
Ben Smith <bh2smith@gmail.com>
2022-07-04 12:40:20 +0200

    make quote parsing more specific by app version

--- src/models/app_data_json_format.rs
+++ src/models/app_data_json_format.rs
@@ -11,9 +11,11 @@ pub struct Referrer {
 }
 
 #[derive(Eq, PartialEq, Clone, Debug, Deserialize, Serialize, Hash)]
-#[serde(untagged)]
+#[serde(tag = "version")]
 pub enum Quote {
+    #[serde(rename = "0.1.0")]
     V1(QuoteV1),
+    #[serde(rename = "0.2.0")]
     V2(QuoteV2),
 }
 
@@ -24,13 +26,11 @@ pub struct QuoteV1 {
     pub sell_amount: U256,
     #[serde(with = "u256_decimal")]
     pub buy_amount: U256,
-    pub version: String,
 }
 
 #[derive(Eq, PartialEq, Clone, Debug, Deserialize, Serialize, Hash)]
 #[serde(rename_all = "camelCase")]
 pub struct QuoteV2 {
-    pub version: String,
     // This value does not need a large uint type
     #[serde(with = "serde_with::rust::display_fromstr")]
     pub slippage_bips: u32,
@@ -114,7 +114,7 @@ mod tests {
                         "version":"6.6.6"
                 },
                 "quote": {
-                    "version": "1.0",
+                    "version": "0.1.0",
                     "sellAmount": "23426235345",
                     "buyAmount": "2341253523453",
                 }
@@ -134,7 +134,6 @@ mod tests {
                     version: "6.6.6".to_string(),
                 }),
                 quote: Some(Quote::V1(QuoteV1 {
-                    version: String::from("1.0"),
                     sell_amount: U256::from_dec_str("23426235345").unwrap(),
                     buy_amount: U256::from_dec_str("2341253523453").unwrap(),
                 })),
@@ -144,7 +143,7 @@ mod tests {
         assert_eq!(json, expected);
     }
     #[test]
-    fn test_loading_json_v4_with_slippage() {
+    fn test_loading_json_v4() {
         let value = json!({
                 "version":"1.2.3",
                 "appCode":"MooSwap",
@@ -157,7 +156,7 @@ mod tests {
                         "version":"6.6.6"
                 },
                 "quote": {
-                    "version": "2.0",
+                    "version": "0.2.0",
                     "slippageBips": "5"
                 }
             }
@@ -176,55 +175,7 @@ mod tests {
                     version: "6.6.6".to_string(),
                 }),
                 quote: Some(Quote::V2(QuoteV2 {
-                    version: String::from("2.0"),
                     slippage_bips: 5,
-                })),
-            }),
-        };
-
-        assert_eq!(json, expected);
-    }
-    #[test]
-    fn test_loading_json_v4_with_amounts() {
-        let value = json!({
-                "version":"1.2.3",
-                "appCode":"MooSwap",
-                "environment": "production",
-                "metadata":{
-                    "environment": "production",
-                    "referrer":{
-                        "kind":"referrer",
-                        "address":"0x8c35B7eE520277D14af5F6098835A584C337311b",
-                        "version":"6.6.6"
-                },
-                "quote": {
-                    "version": "2.0",
-                    "buyAmount": "123456789",
-                    "sellAmount": "123456789101112",
-                }
-            }
-        });
-        let json: AppData = serde_json::from_value(value).unwrap();
-        let expected = AppData {
-            version: "1.2.3".to_string(),
-            app_code: "MooSwap".to_string(),
-            environment: Some("production".to_string()),
-            metadata: Some(Metadata {
-                environment: Some("production".to_string()),
-                referrer: Some(Referrer {
-                    address: "0x8c35B7eE520277D14af5F6098835A584C337311b"
-                        .parse()
-                        .unwrap(),
-                    version: "6.6.6".to_string(),
-                }),
-                // Notice that although we have specified v2 for quote, we fall back on the v1 quote.
-                // This is much cleaner than trying to implement optional fields for each of
-                // the three components and validate that
-                // only one of slippage OR (both of sell/buy amounts are populated)
-                quote: Some(Quote::V1(QuoteV1 {
-                    version: String::from("2.0"),
-                    buy_amount: U256::from_dec_str("123456789").unwrap(),
-                    sell_amount: U256::from_dec_str("123456789101112").unwrap(),
                 })),
             }),
         };
@@ -235,24 +186,22 @@ mod tests {
     #[test]
     fn test_loading_quote() {
         let v1_value = json!({
-            "version": "2.0",
+            "version": "0.1.0",
             "sellAmount": "123",
             "buyAmount": "4567",
         });
         let v2_value = json!({
-            "version": "2.0",
+            "version": "0.2.0",
             "slippageBips": "100"
         });
         let json_1: Quote = serde_json::from_value(v1_value).unwrap();
         let json_2: Quote = serde_json::from_value(v2_value).unwrap();
         let expected_1 = Quote::V1(QuoteV1 {
-            version: String::from("2.0"),
             sell_amount: U256::from_dec_str("123").unwrap(),
             buy_amount: U256::from_dec_str("4567").unwrap(),
         });
 
         let expected_2 = Quote::V2(QuoteV2 {
-            version: String::from("2.0"),
             slippage_bips: 100,
         });
 
