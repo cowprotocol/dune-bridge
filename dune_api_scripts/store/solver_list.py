9fa1253b73b0af019974aea267a533253c317ca4
Ben Smith <bh2smith@gmail.com>
2022-07-08 15:33:32 +0200

    store solver-list

---
+++ dune_api_scripts/store/solver_list.py
@@ -0,0 +1,61 @@
+"""Modifies and executed dune query for today's data"""
+from __future__ import annotations
+
+import dataclasses
+import json
+import os
+from dataclasses import dataclass
+from typing import Any
+
+from duneapi.api import DuneAPI
+from duneapi.types import DuneQuery, Network
+from duneapi.types import Address
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
+
+
+@dataclass
+class Solver:
+    address: str
+    environment: str
+    name: str
+    active: bool
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> Solver:
+        return cls(
+            address=Address(data["address"]).address,
+            environment=data["environment"],
+            name=data["name"],
+            active=data["active"],
+        )
+
+
+def store_solver_list(dune: DuneAPI) -> list[Solver]:
+    # TODO - fetch for both networks and merge JSON content with chainID
+    raw_solver_list = dune.fetch(
+        DuneQuery.from_environment(
+            name="Solver List",
+            description="",
+            raw_sql="select * from gnosis_protocol_v2.view_solvers",
+            network=Network.MAINNET,
+            parameters=[],
+        )
+    )
+    solver_list = [Solver.from_dict(rec) for rec in raw_solver_list]
+
+    filename = os.path.join(os.environ["DUNE_DATA_FOLDER"], "solvers.json")
+    with open(filename, "w+", encoding="utf-8") as f:
+        json.dump(solver_list, f, ensure_ascii=False, indent=4, cls=EnhancedJSONEncoder)
+
+    return solver_list
+
+
+if __name__ == "__main__":
+
+    store_solver_list(dune=DuneAPI.new_from_environment())
