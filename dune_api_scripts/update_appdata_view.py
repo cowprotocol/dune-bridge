8b1f2d5db9fef347061d51f11e74c2346bdc13b4
Ben Smith <bh2smith@gmail.com>
2022-07-12 17:25:58 +0200

    first draft of multi env update

--- dune_api_scripts/update_appdata_view.py
+++ dune_api_scripts/update_appdata_view.py
@@ -1,43 +1,72 @@
 """Modifies and executed dune query for today's data"""
+import argparse
+from enum import Enum
 from os import getenv
 
 from duneapi.api import DuneAPI
-from duneapi.types import DuneQuery, Network
+from duneapi.types import DuneQuery, Network, QueryParameter
 from duneapi.util import open_query
 
 from .utils import app_data_entries
 
-if __name__ == "__main__":
-    # initialize the environment
-    dune = DuneAPI.new_from_environment()
-    VALUES = app_data_entries()
 
-    # build query from VALUES
-    QUERY = open_query("./dune_api_scripts/queries/parsed_app_data.sql").replace(
-        "{{VALUES}}", VALUES
+def refresh(dune: DuneAPI, query: DuneQuery):
+    dune.initiate_query(query)
+    job_id = dune.execute_query(query)
+    dune.get_results(job_id)
+    print(
+        f"{query.name} successfully updated: https://dune.xyz/queries/{query.query_id}"
     )
-    query_id = int(getenv("QUERY_ID_ALL_APP_DATA", "863359"))
-    app_data_query = DuneQuery(
-        name="App Data Mapping",
+
+
+def update_raw_app_data(dune: DuneAPI):
+    values = app_data_entries()
+    query_id = int(getenv("QUERY_ID_RAW_APP_DATA"))
+    query = DuneQuery(
+        name="Raw App Data Mapping",
         description="",
-        raw_sql=QUERY,
+        raw_sql=open_query("./dune_api_scripts/queries/raw_app_data.sql").replace(
+            "{{VALUES}}", values
+        ),
         network=Network.MAINNET,
         parameters=[],
         query_id=query_id,
     )
-    # App hash with referral data as json
+    refresh(dune, query)
+
+
+class Environment(Enum):
+    staging = "barn"
+    production = "prod"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+def update_parsed_app_data(dune: DuneAPI, env: Environment):
+    query_id = int(getenv("QUERY_ID_PARSED_APP_DATA"))
+    query = DuneQuery(
+        name="Parsed App Data",
+        description="",
+        raw_sql=open_query("./dune_api_scripts/queries/parsed_app_data.sql"),
+        network=Network.MAINNET,
+        parameters=[
+            QueryParameter.enum_type("Environment", env.value, ["barn", "prod"])
+        ],
+        query_id=query_id,
+    )
+    refresh(dune, query)
+
+
+if __name__ == "__main__":
+    dune_connection = DuneAPI.new_from_environment()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--environment", type=Environment, choices=list(Environment), required=True
+    )
+    args = parser.parse_args()
     try:
-        dune.initiate_query(app_data_query)
-        dune.execute_query(app_data_query)
-        print(
-            f"app data successfully updated at https://dune.xyz/queries/{query_id}"
-        )
-    except SystemExit as err:
-        # This is an issue with error handling on duneapi side:
-        # https://github.com/bh2smith/duneapi/issues/48
-        print("Failed likely due to dune login credentials", err)
-    except Exception as err:  # pylint:disable=broad-except
-        # TODO - this is only temporary till we can fix the above exception handling...
-        #  Essentially, we allow failure so not to disturb the processes.
-        print("Unhandled exception", err)
-    # Check out the raw results here: https://dune.xyz/queries/863359
+        update_raw_app_data(dune_connection)
+        update_parsed_app_data(dune_connection, args.environment)
+    except RuntimeError as err:
+        print("Failed to update due to", err)
