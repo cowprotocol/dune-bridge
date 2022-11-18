137950008c8342aef15a06eb3e0f1708bbadd0b5
Ben Smith <bh2smith@gmail.com>
2022-11-18 15:41:42 +0100

    handling missing values

--- pysrc/main.py
+++ pysrc/main.py
@@ -1,5 +1,6 @@
 """Main Entry point for app_hash sync"""
 import asyncio
+import json
 import logging.config
 import os
 
@@ -14,61 +15,118 @@ logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
 log.setLevel(logging.DEBUG)
 
 
-def fetch_and_filter(
-    dune_results: list[DuneRecord],
-    max_retries: int = 3,
-) -> tuple[list[DuneRecord], list[DuneRecord]]:
-    """
-    Run loop fetching app_data for hashes,
-    separates into (found and not found), returning the pair.
-    """
-    found: list[dict[str, str]] = []
-    not_found: list[dict[str, str]] = []
-    # Drain the dune_results into "found" and "not found" categories
-    while dune_results:
-        row = dune_results.pop()
-        app_hash = row["app_hash"]
-        cid = Cid(app_hash)
-        app_data = cid.get_content(max_retries)
+GIVE_UP_THRESHOLD = 10
 
-        # Here it would be nice if python we more like rust!
-        if app_data is not None:
-            # Row is modified and added found items
-            log.debug(f"Found content for {row['app_hash']} at CID {cid}")
-            row["content"] = app_data
-            found.append(row)
-        else:
-            # Unmodified row added to not_found items
-            log.debug(
-                f"No content found for {row['app_hash']} at CID {cid} after {max_retries} retries"
-            )
-            not_found.append(row)
 
-    return found, not_found
+class RecordHandler:  # pylint:disable=too-few-public-methods
+    """
+    This class is responsible for consuming new dune records and missing values from previous runs
+    it attempts to fetch content for them and filters them into "found" and "not found" as necessary
+    """
+
+    def __init__(self, new_rows: list[DuneRecord], missing_values: list[DuneRecord]):
+        self.found: list[dict[str, str]] = []
+        self.not_found: list[dict[str, str]] = []
+
+        self.new_rows = new_rows
+        self.missing_values = missing_values
+
+    def _handle_new_records(self, max_retries: int) -> None:
+        # Drain the dune_results into "found" and "not found" categories
+        while self.new_rows:
+            row = self.new_rows.pop()
+            app_hash = row["app_hash"]
+            cid = Cid(app_hash)
+            app_data = cid.get_content(max_retries)
+
+            # Here it would be nice if python we more like rust!
+            if app_data is not None:
+                # Row is modified and added found items
+                log.debug(f"Found content for {app_hash} at CID {cid}")
+                row["content"] = app_data
+                self.found.append(row)
+            else:
+                # Unmodified row added to not_found items
+                log.debug(
+                    f"No content found for {app_hash} at CID {cid} after {max_retries} retries"
+                )
+                # Dune Records are string dicts.... :(
+                row["attempts"] = str(max_retries)
+                self.not_found.append(row)
+
+    def _handle_missing_records(self, max_retries: int) -> None:
+        while self.missing_values:
+            row = self.missing_values.pop()
+            app_hash = row["app_hash"]
+            cid = Cid(app_hash)
+            app_data = cid.get_content(max_retries)
+            attempts = int(row["attempts"]) + max_retries
+
+            if app_data is not None:
+                log.debug(
+                    f"Found previously missing content hash {row['app_hash']} at CID {cid}"
+                )
+                self.found.append(
+                    {
+                        "app_hash": app_hash,
+                        "first_seen_block": row["first_seen_block"],
+                        "content": app_data,
+                    }
+                )
+            elif app_data is None and attempts > GIVE_UP_THRESHOLD:
+                log.debug(
+                    f"No content found after {attempts} attempts for {app_hash} assuming NULL."
+                )
+                self.found.append(
+                    {
+                        "app_hash": app_hash,
+                        "first_seen_block": row["first_seen_block"],
+                        "content": json.dumps({}),
+                    }
+                )
+            else:
+                row.update({"attempts": str(attempts)})
+                self.not_found.append(row)
+
+    def fetch_content_and_filter(
+        self, max_retries: int
+    ) -> tuple[list[DuneRecord], list[DuneRecord]]:
+        """
+        Run loop fetching app_data for hashes,
+        separates into (found and not found), returning the pair.
+        """
+        self._handle_new_records(max_retries)
+        log.info(
+            f"Attempting to recover missing {len(self.missing_values)} records from previous run"
+        )
+        self._handle_missing_records(max_retries)
+        return self.found, self.not_found
 
 
 async def main(dune: DuneFetcher) -> None:
     """App Hash Sync Logic"""
     # TODO - use runtime config values here:
-    fname, column = "sync_block", "last_synced_block"
+    fname, column, missing_fname, max_retries = (
+        "sync_block",
+        "last_synced_block",
+        "missing_app_hashes",
+        3,
+    )
     block_range = await dune.app_hash_block_range(fname, column)
-
     dune_results = await dune.get_app_hashes(block_range)
 
-    # TODO - although it is unlikely we will ever "cold-start",
-    #  it seems that storing all this data in memory could be problematic
-    #  e.g. program fails and all progress is lost, etc...
-    #  It might be nice to append the results directly to the stream as they are found.
-    #  I am going to implement append anyway (for the missing records)
-    found, not_found = fetch_and_filter(dune_results)
+    try:
+        missing_records = dune.file_manager.load_ndjson(missing_fname)
+    except FileNotFoundError:
+        missing_records = []
+
+    data_handler = RecordHandler(new_rows=dune_results, missing_values=missing_records)
+    found, not_found = data_handler.fetch_content_and_filter(max_retries)
 
     # Write the most recent data and also record the block_from,
     # so that next run will know where to start
-    dune.file_manager.write_ndjson(found, f"app_hashes_{block_range.block_to}")
-    # TODO - "append" missing to an existing file.
-    dune.file_manager.write_ndjson(
-        not_found, f"missing_app_hashes_{block_range.block_to}"
-    )
+    dune.file_manager.write_ndjson(found, f"app_content_{block_range.block_to}")
+    dune.file_manager.write_ndjson(not_found, missing_fname)
     # Write last sync block only after the data has been written.
     dune.file_manager.write_csv([{column: str(block_range.block_to)}], fname)
 
