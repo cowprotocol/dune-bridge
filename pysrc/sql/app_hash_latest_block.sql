b80d800de044d00d8037788bb2789aeba84bff1d
Ben Smith <bh2smith@gmail.com>
2022-11-21 16:34:48 +0100

    delete dune sync project

--- pysrc/sql/app_hash_latest_block.sql
+++
@@ -1,4 +0,0 @@
--- https://dune.com/queries/1615490
-select
-  max(call_block_number) as latest_block
-from gnosis_protocol_v2_ethereum.GPv2Settlement_call_settle
--- pysrc/sync/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Re-exported sync methods."""
-from .app_data import sync_app_data
