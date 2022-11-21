6bb6584468217e44181ef51b7169e02b64ab4ee6
Ben Smith <bh2smith@gmail.com>
2022-11-21 13:34:31 +0100

    connect aws upload to main app data fetch process

--- pysrc/sync/app_data.py
+++ pysrc/sync/app_data.py
@@ -1,6 +1,8 @@
 """Main Entry point for app_hash sync"""
 import json
 import logging.config
+import os.path
+from collections import namedtuple
 
 from dune_client.file.interface import FileIO
 from dune_client.types import DuneRecord
@@ -9,22 +11,39 @@ from pysrc.environment import OUT_DIR
 from pysrc.fetch.dune import DuneFetcher
 from pysrc.fetch.ipfs import Cid
 from pysrc.models.block_range import BlockRange
+from pysrc.post.aws import upload_file, get_s3_client
 
 log = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
 log.setLevel(logging.DEBUG)
 
-
+MAX_RETRIES = 3
 GIVE_UP_THRESHOLD = 10
 
+Files = namedtuple("Files", "table_name, missing_files_name, sync_file, sync_column")
+APP_DATA_FILES = Files(
+    table_name="app_data",
+    missing_files_name="missing_app_hashes.json",
+    sync_file="sync_block.csv",
+    sync_column="last_synced_block",
+)
 
-class RecordHandler:  # pylint:disable=too-few-public-methods
+
+class RecordHandler:
     """
     This class is responsible for consuming new dune records and missing values from previous runs
     it attempts to fetch content for them and filters them into "found" and "not found" as necessary
     """
 
-    def __init__(self, new_rows: list[DuneRecord], missing_values: list[DuneRecord]):
+    def __init__(
+        self,
+        new_rows: list[DuneRecord],
+        missing_values: list[DuneRecord],
+        block_range: BlockRange,
+    ):
+
+        self.block_range = block_range
+
         self.found: list[dict[str, str]] = []
         self.not_found: list[dict[str, str]] = []
 
@@ -105,53 +124,103 @@ class RecordHandler:  # pylint:disable=too-few-public-methods
         self._handle_missing_records(max_retries)
         return self.found, self.not_found
 
+    def write_to_disk(self, file_manager: FileIO, filename: str) -> None:
+        """
+        Does all appropriate file writes for a single run of the app data sync job
+        Write new records, missing records and last sync block.
+        """
+        # Write the most recent data and also record the block_from,
+        # so that next run will know where to start
+        file_manager.write_ndjson(self.found, filename)
+        # When not_found is empty, we want to overwrite the file (hence skip_empty=False)
+        # This happens when all records in the file have attempts exceeding GIVE_UP_THRESHOLD
+        file_manager.write_ndjson(
+            self.not_found, APP_DATA_FILES.missing_files_name, skip_empty=False
+        )
+        # Write last sync block only after the data has been written.
+        file_manager.write_csv(
+            data=[{APP_DATA_FILES.sync_column: str(self.block_range.block_to)}],
+            name=APP_DATA_FILES.sync_file,
+        )
 
-async def sync_app_data(dune: DuneFetcher) -> None:
-    """App Data Sync Logic"""
 
-    file_manager = FileIO(OUT_DIR / "app_data")
-    # TODO - use runtime config values here:
-    sync_block_file, column, missing_fname, max_retries = (
-        "sync_block.csv",
-        "last_synced_block",
-        "missing_app_hashes.json",
-        3,
-    )
+async def get_block_range(
+    file_manager: FileIO, dune: DuneFetcher, last_block_file: str, column: str
+) -> BlockRange:
+    """
+    Constructs a block range object
+    block_from is fetched from the last sync block (via file_manager)
+    block_to is fetched from Dune via query results.
+    """
+
     block_from = 12153262  # Genesis block
     try:
-        block_from = int(file_manager.load_singleton(sync_block_file, "csv")[column])
+        block_from = int(file_manager.load_singleton(last_block_file, "csv")[column])
     except FileNotFoundError:
         log.warning(
-            f"block range file {sync_block_file} not found, using genesis block {block_from}"
+            f"block range file {last_block_file} not found, using genesis block {block_from}"
         )
     except KeyError as err:
         message = (
-            f"block range file {sync_block_file} does not contain column header {column}, "
+            f"block range file {last_block_file} does not contain column header {column}, "
             f"exiting to avoid duplication"
         )
         log.error(message)
         raise RuntimeError(message) from err
-    block_range = BlockRange(
+
+    return BlockRange(
         # TODO - could be replaced by Dune Query on the app_data table (once available).
         #  https://github.com/cowprotocol/dune-bridge/issues/42
         block_from,
         block_to=await dune.latest_app_hash_block(),
     )
-    dune_results = await dune.get_app_hashes(block_range)
 
+
+def get_missing_data(file_manager: FileIO, missing_fname: str) -> list[DuneRecord]:
+    """
+    Loads missing records from file (aka previous run) if there are any.
+    Otherwise, assumes there are none.
+    """
     try:
-        missing_records = file_manager.load_ndjson(missing_fname)
+        return file_manager.load_ndjson(missing_fname)
     except FileNotFoundError:
-        missing_records = []
+        return []
 
-    data_handler = RecordHandler(new_rows=dune_results, missing_values=missing_records)
-    found, not_found = data_handler.fetch_content_and_filter(max_retries)
 
-    # Write the most recent data and also record the block_from,
-    # so that next run will know where to start
-    file_manager.write_ndjson(found, f"cow_{block_range.block_to}.json")
-    # When not_found is empty, we want to overwrite the file (hence skip_empty=False)
-    # This happens when all records in the file have attempts exceeding GIVE_UP_THRESHOLD
-    file_manager.write_ndjson(not_found, missing_fname, skip_empty=False)
-    # Write last sync block only after the data has been written.
-    file_manager.write_csv([{column: str(block_range.block_to)}], sync_block_file)
+async def sync_app_data(dune: DuneFetcher) -> None:
+    """App Data Sync Logic"""
+    table_name = APP_DATA_FILES.table_name
+    file_manager = FileIO(OUT_DIR / table_name)
+    block_range = await get_block_range(
+        file_manager,
+        dune,
+        last_block_file=APP_DATA_FILES.sync_file,
+        column=APP_DATA_FILES.sync_column,
+    )
+
+    data_handler = RecordHandler(
+        new_rows=await dune.get_app_hashes(block_range),
+        missing_values=get_missing_data(
+            file_manager, missing_fname=APP_DATA_FILES.missing_files_name
+        ),
+        block_range=block_range,
+    )
+    found, not_found = data_handler.fetch_content_and_filter(MAX_RETRIES)
+
+    content_filename = f"cow_{block_range.block_to}.json"
+    data_handler.write_to_disk(file_manager, filename=content_filename)
+
+    if len(found) > 0:
+        success = upload_file(
+            s3_client=get_s3_client(os.environ["AWS_IAM_ROLE"]),
+            file_name=os.path.join(file_manager.path, content_filename),
+            bucket=os.environ["AWS_BUCKET"],
+            object_key=f"{table_name}/{content_filename}",
+        )
+        if success:
+            log.info(
+                f"App Data Sync for block range {BlockRange} complete: "
+                f"synced {len(found)} records with {len(not_found)} missing"
+            )
+    else:
+        log.info(f"No new App Data for block range {BlockRange}: no sync necessary")
